5ef5643a485813fdbcee7c5f748fb16039f79d2f
qaissaada <qais.m.saada@gmail.com>
2021-11-24 15:21:10 +0100

    clean up

---
+++ javascript/javascript2/week1/js2week1class.js
@@ -0,0 +1,61 @@
+/*const favFood = ["Makloba", "Mansaf", "Ramen","Pasta"]
+
+const newFood = document.getElementById("Food");
+
+for (let i = 0; i < newFood.length; i++) {
+    const li = 
+
+    
+} */
+
+/*const podcasts = [{
+    name: 'The mystery om of Johan Klausen Varbourg',
+    imageUrl: 'https://picsum.photos/536/354'
+},
+{
+    name: 'Tips about dogs with small legs',
+    imageUrl: 'https://picsum.photos/536/354'
+},
+{
+    name: 'You, me, we and us',
+    imageUrl: 'https://picsum.photos/536/354'
+},
+{
+    name: 'Breakfast news - Dinner edition'
+}
+];
+
+const podcastsul = document.getElementById("podcastID")
+
+for (let i = 0; i < podcasts.length; i++) {
+const podcastsli = document.createElement("li")
+const podcastsH1 = document.createElement("h1")
+//const podcasts = podcasts[i]
+podcastsH1.innerHTML = podcasts[i]
+podcastsli.appendChild(podcastsH1)
+
+if (podcasts.imageUrl === undefined) {
+    const image = document.createElement("img")
+
+image.setAttribute("src", podcasts.imageUrl)}
+image.setAttribute("alt", "pdcast name: ${podcast.name}")
+
+podcastsli.appendChild(image)
+podcastsul.appendChild(podcastsli)
+}
+
+document.querySelector(body).appendChild(podcastsul); */
+
+
+
+
+/*document.getElementById("myBtn").addEventListener("click", function(event) {
+    document.getElementById("myBtn").innerHTML = "It is working";
+})*/
+
+
+
+
+
+
+
